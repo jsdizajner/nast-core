05b91cf7d5f672fb4ba19231c7f14d4addb0ec39
Július Sipos <33227696+jsdizajner@users.noreply.github.com>
2023-12-06 13:50:33 +0100

    Initial commit

---
+++ nast-core.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: NAST Core
+ * Plugin URI: https://nast.sk
+ * Description: Core Business functionality
+ * Version: dev.0.0.1
+ * Author: SIPOS.DIGITAL
+ * Author URI: https://sipos.digital
+ * Text Domain: nast-core
+ * Requires PHP: 7.4
+ */
+
+defined( 'ABSPATH' ) || exit;
+
