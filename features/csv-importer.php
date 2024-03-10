9f712789477c6738afa2f8fd2f2294fb02f908c1
simonmarsalek <simon.marsalek.sk@gmail.com>
2024-03-10 20:18:21 +0100

    Added CSV Importer fix/addition

---
+++ features/csv-importer.php
@@ -0,0 +1,94 @@
+<?php
+namespace NAST_CORE\Features;
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Add custom field for product_brand taxonomy in Woocommerce CSV Importer
+ * 
+ * This is only addition to Woocommerce Importer, which does nothing by itself
+ */
+
+class CSV_Importer
+{
+    // This is the column name in csv Importer table which we want to create product_brand from
+    public const PRODUCTBRAND_COLUMN = '_supplier';
+
+
+    // Add hooks to add custom table column
+    public static function init()
+    {
+        // from Woocommerce docs :
+
+        /**
+         * Register the 'Custom Column' column in the importer.
+         *
+         * @param array $options
+         * @return array $options
+         */
+        function nastcsvimporter__add_column_to_importer( $options ) {
+
+            // column slug => column name
+            $options[  CSV_Importer::PRODUCTBRAND_COLUMN  ] = 'Supplier';
+
+            return $options;
+        }
+        add_filter( 'woocommerce_csv_product_import_mapping_options', 'nastcsvimporter__add_column_to_importer' );
+
+        /**
+         * Add automatic mapping support for 'Custom Column'. 
+         * This will automatically select the correct mapping for columns named 'Custom Column' or 'custom column'.
+         *
+         * @param array $columns
+         * @return array $columns
+         */
+        function nastcsvimporter__add_column_to_mapping_screen( $columns ) {
+            
+            // potential column name => column slug
+            $columns['Supplier'] = CSV_Importer::PRODUCTBRAND_COLUMN;
+            $columns['supplier'] = CSV_Importer::PRODUCTBRAND_COLUMN;
+
+            return $columns;
+        }
+        add_filter( 'woocommerce_csv_product_import_mapping_default_columns', 'nastcsvimporter__add_column_to_mapping_screen' );
+
+        /**
+         * Process the data read from the CSV file.
+         * This just saves the value in meta data, but you can do anything you want here with the data.
+         *
+         * @param WC_Product $object - Product being imported or updated.
+         * @param array $data - CSV data read for the product.
+         * @return WC_Product $object
+         */
+        function nastcsvimporter__process_import( $object, $data ) {
+            
+            if ( ! empty( $data[  CSV_Importer::PRODUCTBRAND_COLUMN  ] ) ) {
+                
+                /**
+                 * Do what we want with our custom column here..
+                 */
+                $supplier = $data[  CSV_Importer::PRODUCTBRAND_COLUMN  ];
+                $product_id = $object->get_id();
+
+                if ( taxonomy_exists('product_brand') && ! has_term( $supplier, 'product_brand', $product_id ) ){
+                    // Create term - product brand
+                    $result = wp_set_object_terms( $product_id, $supplier, 'product_brand', true );
+
+                    if ( is_wp_error($result) ) {
+                        // We can throw Exception here because it will be catched in Woocommerce Importer loop
+                        throw new Exception('[Nast-Core] : Failed to add product_brand for this product.');
+                    }
+
+                }
+
+            }
+
+            return $object;
+        }
+        add_filter( 'woocommerce_product_import_pre_insert_product_object', 'nastcsvimporter__process_import', 10, 2 );
+
+    }
+}
+ 
+
+
+CSV_Importer::init();
--- nast-core.php
+++ nast-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: NAST Core
  * Plugin URI: https://nast.sk
  * Description: Core Business functionality
- * Version: dev.0.0.1
+ * Version: dev.0.0.2
  * Author: SIPOS.DIGITAL
  * Author URI: https://sipos.digital
  * Text Domain: nast-core
@@ -11,6 +11,10 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+
+define('NAST_CORE_DIR', plugin_dir_path(__FILE__));
+define('NAST_CORE_URL', plugin_dir_url(__FILE__));
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
@@ -26,3 +30,5 @@ $myUpdateChecker->setBranch('main');
 $core_plugins = [];
 
 
+require_once NAST_CORE_DIR .'/features/order-status/order-status.php';
+require_once NAST_CORE_DIR .'/features/csv-importer.php';
