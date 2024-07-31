1c25bcb5ce8be1bb3885040ce5065a190fe3eb20
Július Sipos <33227696+jsdizajner@users.noreply.github.com>
2024-07-31 11:33:53 +0200

    Checkout shipment adjustments

--- nast-core.php
+++ nast-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: NAST Core
  * Plugin URI: https://nast.sk
  * Description: Core Business functionality
- * Version: 1.2.6
+ * Version: 1.2.7
  * Author: SIPOS.DIGITAL
  * Author URI: https://sipos.digital
  * Text Domain: nast-core
